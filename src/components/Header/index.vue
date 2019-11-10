3cdc71d138f1bfe9ed9f0e08fc670f3dc948fffd
kyrie3 <info@kyrie.cloud>
2019-11-10 17:42:14 +0900

    fix

---
+++ src/components/Header/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase">
+      <span class="font-weight-light">Favorite Movie</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <Login />
+    <Signup />
+  </v-toolbar>
+</template>
+
+<script>
+import Signup from "./Signup";
+import Login from "./Login";
+export default {
+  components: { Signup, Login }
+};
+</script>
