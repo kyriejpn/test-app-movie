699ddb8996e0d2f26eb6d356e4bf7d874b925c1e
kyrie3 <info@kyrie.cloud>
2019-11-10 21:43:53 +0900

    update:vee-valitateを追加しつつもエラーが出ない設定

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
 import './plugins/vuetify';
 import App from './App.vue';
+//import VeeValidate, { Validator } from 'vee-validate';
 import router from './router'; // 追記
 import './plugins/vuetify';
 import vuetify from './plugins/vuetify';
+import { extend /*, ValidationProvider, ValidationObserver */} from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+
+// バリデーションルール
+extend('required', required);
+extend('email', email);
+
 
 Vue.config.productionTip = false;
 
